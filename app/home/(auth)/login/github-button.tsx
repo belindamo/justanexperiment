41a523e097dc383e1f8aed25fd552237d9661c80
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2024-04-17 20:03:57 -0500

    Rename login-button to github-button

---
+++ app/home/(auth)/login/github-button.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Github from "@/components/icons/github";
+import LoadingDots from "@/components/icons/loading-dots";
+import { signIn } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
+import { useState, useEffect } from "react";
+import { toast } from "sonner";
+
+export default function LoginButton() {
+  const [loading, setLoading] = useState(false);
+
+  // Get error message added by next/auth in URL.
+  const searchParams = useSearchParams();
+  const error = searchParams?.get("error");
+
+  useEffect(() => {
+    const errorMessage = Array.isArray(error) ? error.pop() : error;
+    errorMessage && toast.error(errorMessage);
+  }, [error]);
+
+  return (
+    <button
+      disabled={loading}
+      onClick={() => {
+        setLoading(true);
+        signIn("github");
+      }}
+      className={`${loading
+          ? "cursor-not-allowed bg-stone-50 dark:bg-stone-800"
+          : "bg-white hover:bg-stone-50 active:bg-stone-100 dark:bg-black dark:hover:border-white dark:hover:bg-black"
+        } group my-2 flex h-10 w-full items-center justify-center space-x-2 rounded-md border border-stone-200 transition-colors duration-75 focus:outline-none dark:border-stone-700`}
+    >
+      {loading ? (
+        <LoadingDots color="#A8A29E" />
+      ) : (
+        <>
+          <Github className="mr-2 h-4 w-4" />
+          <p className="text-sm font-medium text-stone-600 dark:text-stone-400">
+            Login with GitHub
+          </p>
+        </>
+      )}
+    </button>
+  );
+}
